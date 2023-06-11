cfb21b26cb81b59dbc0c61feeace42e1bbb665b0
ferrreo <harderthanfire@gmail.com>
2023-06-11 20:23:23 +0100

    Initial version

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 # Sign the packages
-dpkg-sig --sign builder ./output/pika-kde-desktop*.deb
+dpkg-sig --sign builder ./output/pika-hyprland-desktop*.deb
 
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-kde-desktop*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-hyprland-desktop*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-kde-desktop*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-hyprland-desktop*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
